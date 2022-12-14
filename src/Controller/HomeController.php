197d58bf9b2a1d028158d74f5d0aa36eb9635d72
LorenzoDv <lorenzo.devichi@hotmail.fr>
2022-12-14 09:42:00 +0100

    update catégorie

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
 use App\Entity\Post;
+use App\Repository\CategoryRepository;
 use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,32 +13,41 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 
 {
+
+
     #[Route('/' , name: 'home')]
-    public function index(PostRepository  $postReposiory): Response
+    public function index(): Response
     {
         $name = 'Lorenzo';
-    
-
-        
-
         return $this->render('base.html.twig', [
             'name' =>  $name,
-        
         ]);
     }
 
 
     #[Route('/Tous-les-articles' , name: 'Tous-les-articles')]
-    public function article(PostRepository  $postReposiory): Response
+    public function article(PostRepository  $postReposiory, CategoryRepository $CategoryRepository): Response
     {
         $name = 'Lorenzo';
         $posts = $postReposiory->findAll();
+        $category = $CategoryRepository->findAll();
 
         
 
         return $this->render('home/index.html.twig', [
             'name' =>  $name,
-            'posts' => $posts
+            'posts' => $posts,
+            'category' => $category
         ]);
+    }
+    
+    #[Route('/Tous-les-articles/category/{id<[0-9]+>}', name:'index_by_category')]
+    public function indexByCategory(Category $category, CategoryRepository $CategoryRepository)
+
+    {
+          return $this->render('home/index.html.twig', [
+            'posts' => $category->getPosts(),
+            'category' => $CategoryRepository->findall()
+        ]); 
     }
 }
--- src/DataFixtures/PostFixtures.php
+++ src/DataFixtures/PostFixtures.php
@@ -6,20 +6,27 @@ namespace App\DataFixtures;
 use App\Entity\Post;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+
 use Faker;
 
-class AppFixtures extends Fixture
+class PostFixtures extends Fixture
 {
-    public function load(ObjectManager $manager)
+    public function load(ObjectManager $manager) : void
     {
         $faker = Faker\Factory::create('fr_FR');
         // create 20 products! Bam!
-        for ($i = 0; $i < 20; $i++) {
+        for ($i = 0; $i < 10; $i++) {
             $post = new Post();
             $post->setTitle($faker->words(rand(3,10), true));
             $post->setAuthor($faker->name());
             $post->setDescription($faker->paragraphs(rand(1,4),true));
             $post->setImage('https://placeimg.com/300/300/tech');
+
+            for($j = 0; $j < rand(1, 3); $j++) {
+                $post->addCategory($this->getReference('category' . rand(0, 4)));
+            }
+           
+
             $manager->persist($post);
         }
 
